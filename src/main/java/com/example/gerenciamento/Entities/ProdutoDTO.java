017e6eba7fd4ffc7ecb5081addd39c052bbc2a5a
rodrigoortolani <rodrigoortolani@rodrigoortolani.rodrigoortolani>
2025-05-15 19:14:27 -0300

    teste deploy

--- src/main/java/com/example/gerenciamento/Entities/ProdutoDTO.java
+++ src/main/java/com/example/gerenciamento/Entities/ProdutoDTO.java
@@ -3,9 +3,12 @@ package com.example.gerenciamento.Entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+
 public class ProdutoDTO {
     private Long id;
     private String nome;
